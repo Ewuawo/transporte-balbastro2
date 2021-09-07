be480bc1242060cba026980050147589d435e317
Ewuawo <ewuawo@gmail.com>
2021-09-06 23:40:57 -0300

    practico2

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+function NavBar() {
+    return (
+        <div id="navegador">
+<ul>
+<li>Transporte Balbastro</li>
+<li><a href="#">Inicio</a></li>
+<li><a href="#">Nosotros</a></li>
+<li><a href="#">Servicios</a></li>
+<li><a href="#">Contacto</a></li>
+</ul>
+<h2>Practico 2</h2>
+</div>
+
+    );
+}
+export default NavBar;
