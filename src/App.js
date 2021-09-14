19618380e09a1503692802225b52268aa132df63
Ewuawo <ewuawo@gmail.com>
2021-09-13 21:18:56 -0300

    updatemuymejorable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+//import Header from './components/Header';
+import '../src';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,11 @@ function App() {
         >
           Learn React
         </a>
+=======
+        <NavBar />
+>>>>>>> Stashed changes
       </header>
+      <ItemListContainer />
     </div>
   );
 }
